7dafd3f66953754125aa8e324760703d55767f35
Sasha1448 <agames1448@gmail.com>
2022-05-26 22:33:07 +0300

    [Added] Controller

---
+++ UnitTesting/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnitTesting.Models;
+using System.Linq;
+
+namespace UnitTesting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SimpleRepository _repository = SimpleRepository.simpleRepository;
+        public IActionResult Index() => View(_repository.Products.Where(p => p?.Price< 50));
+        
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            _repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
