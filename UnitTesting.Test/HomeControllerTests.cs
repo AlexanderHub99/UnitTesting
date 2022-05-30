a3ae95eda07fdd4175a5e9b19d3256c4747f6c2b
Sasha1448 <agames1448@gmail.com>
2022-05-31 01:04:25 +0300

    [ref] Unit test HomeController.cs

--- UnitTesting.Test/HomeControllerTests.cs
+++ UnitTesting.Test/HomeControllerTests.cs
@@ -3,33 +3,39 @@ using Microsoft.AspNetCore.Mvc;
 using UnitTesting.Controllers;
 using UnitTesting.Models;
 using Xunit;
+using Moq;
 
 namespace UnitTesting.Test
 {
     public class HomeControllerTests
     {
-        public class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; set; }
-            public void AddProduct(Product p) { }
-        }
-
         [Theory]
         [ClassData(typeof(ProductTestData))]
         public void IndexActionModelIsComplete(Product[] products)
         {
-            HomeController controller = new HomeController();
-            controller.repository = new ModelCompleteFakeRepository
-            {
-                Products = products,
-            };
-    
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products)
+                .Returns(products);
+
+            var controller = new HomeController {repository = mock.Object};
+
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
-    
-            Assert.Equal(controller.repository.Products, model, Comparer.Get<Product>((p1, p2)
-                => p1.Name == p2.Name && p1.Price == p2.Price));
+
+            Assert.Equal(controller.repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
+                && p1.Price == p2.Price));
         }
-        
+
+        [Fact]
+        public void RepositoryProductCalledOne()
+        {
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products)
+                .Returns(new[] {new Product {Name = "P1", Price = 100}});
+            var controller = new HomeController {repository = mock.Object};
+
+            var result = controller.Index();
+
+            mock.VerifyGet(m => m.Products, Times.Once);
+        }
     }
 }
-
